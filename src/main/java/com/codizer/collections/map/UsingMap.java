3d262d2e7ba7ceed881f0215477633b920148322
Adrian Ortiz <adrianortizmartinez@gmail.com>
2023-06-14 07:55:06 -0600

    Using Map with put and get

---
+++ src/main/java/com/codizer/collections/map/UsingMap.java
@@ -0,0 +1,19 @@
+package com.codizer.collections.map;
+
+import java.util.HashMap;
+
+public class UsingMap {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Person> mapa = new HashMap<>();
+        mapa.put("9", new Person("9", "pepe", 20, 1));
+        mapa.put("2", new Person("2", "pepe", 20, 2));
+        mapa.put("3", new Person("3", "pepe", 20, 3));
+        mapa.put("7", new Person("7", "pepe", 20, 4));
+
+        System.out.println(mapa.get("2"));
+        System.out.println(mapa.get("2").getName());
+
+    }
+}
