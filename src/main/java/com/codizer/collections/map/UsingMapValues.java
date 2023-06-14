d0dadea42a52a97d9d08aebf6ee10b842c8ca04f
Adrian Ortiz <adrianortizmartinez@gmail.com>
2023-06-14 08:05:38 -0600

    Using HashMap and get Values

---
+++ src/main/java/com/codizer/collections/map/UsingMapValues.java
@@ -0,0 +1,25 @@
+package com.codizer.collections.map;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+
+public class UsingMapValues {
+
+    public static void main(String[] args) {
+
+        HashMap<String, Person> mapa = new HashMap<>();
+        mapa.put("9", new Person("9", "pepe", 20, 1));
+        mapa.put("2", new Person("2", "juan", 21, 2));
+        mapa.put("3", new Person("3", "miguel", 22, 3));
+        mapa.put("7", new Person("7", "antonio", 24, 4));
+
+        Collection<Person> collection = mapa.values();
+
+        for (Person p: collection) {
+            System.out.println(p.getDni());
+            System.out.println(p.getName());
+        }
+
+    }
+}
